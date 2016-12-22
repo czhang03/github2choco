d7a078568e7b6a9e5dc2f1c4c990b57af134f0e0
Zhang Cheng <13501393281@163.com>
2016-12-22 10:30:34 +0000

    add function Update-ChocoPackag

--- github2choco.psm1
+++ github2choco.psm1
@@ -4,6 +4,48 @@ Get-ChildItem ./Helpers | ForEach-Object {. "./Helpers/$_"}
 Get-ChildItem ./PackageWriters | ForEach-Object {. "./PackageWriters/$_"}
 
 
+function Update-ChocoPackage {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $packageName,
+        [Parameter(Mandatory = $false)]
+        [switch] $Force
+    )
+    
+    begin 
+    {
+        $profile = Read-LocalProfile
+    }
+    
+    process
+    {
+        # regular log
+        Write-Host ''
+        Write-Host ''
+
+        # verbose log
+        Write-Verbose "updating Package $packageName" 
+        Write-Verbose "the package Type is: $($profile.$packageName.packageType)"
+        Write-Verbose "the package Local Version is: $($profile.$packageName.version)"
+        Write-Verbose "the package github repo is: $($profile.$packageName.githubRepo)"
+        
+        switch ($profile.$packageName.packageType) {
+            'installer' {Update-InstallerChocoPackage -packageName $packageName -Force $Force}
+            'vsix' {Update-VsixChocoPackage -packageName $packageName -Force $Force}
+            'webFile' {Update-WebFileChocoPackage -packageName $packageName -Force $Force}
+            'zip' {Update-ZipChocoPackage -packageName $packageName -Force $Force}
+            Default {Write-Error "Package type not valid"}
+        }
+    }
+    
+    end
+    {
+        
+    }
+}
+
+
 function Update-AllChocoPackage {
     [CmdletBinding()]
     param(
@@ -24,24 +66,7 @@ function Update-AllChocoPackage {
         {
             foreach ($packageName in $packageNames) 
             {
-
-                # regular log
-                Write-Host ''
-                Write-Host ''
-
-                # verbose log
-                Write-Verbose "updating Package $packageName" 
-                Write-Verbose "the package Type is: $($profile.$packageName.packageType)"
-                Write-Verbose "the package Local Version is: $($profile.$packageName.version)"
-                Write-Verbose "the package github repo is: $($profile.$packageName.githubRepo)"
-                
-                switch ($profile.$packageName.packageType) {
-                    'installer' { Update-InstallerChocoPackage -packageName $packageName -Force}
-                    'vsix' {Update-VsixChocoPackage -packageName $packageName -Force}
-                    'webFile' {Update-WebFileChocoPackage -packageName $packageName -Force}
-                    'zip' {Update-ZipChocoPackage -packageName $packageName -Force}
-                    Default {Write-Error "Package type not valid"}
-                }
+                Update-ChocoPackage -packageName $packageName -Force
             }
         }
         
